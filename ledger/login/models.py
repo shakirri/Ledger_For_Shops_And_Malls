931da79d7877777a79b323c5e22ea6c279a2552a
shakirri <87424947+shakirri@users.noreply.github.com>
2021-08-13 17:31:42 +0600

    Completed Login Feature along with error handling
    Wrong Username or Password Handled

--- ledger/login/models.py
+++ ledger/login/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    userid = models.CharField(max_length=50)
+    passw = models.CharField(max_length=50)
--- ledger/login/views.py
+++ ledger/login/views.py
@@ -1,13 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Users
 
 # Create your views here.
 def login1(request):
     return render(request, "login.html")
 
 def checkpass(request):
-
     val_user= request.POST['username']
     val_pass= request.POST['password']
-    
-    return render(request, "home.html")
+    x=Users.objects.filter(userid=val_user)
+    if len(x)==0:
+        return render(request, "error.html");
+    a=Users.objects.get(userid=val_user)
+    if(a.passw==val_pass):
+        return render(request, "home.html");
+    else:
+        return render(request, "error.html");
