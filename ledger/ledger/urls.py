f95cc5940aac20116ebd981065c0ebea25793de9
shakirri <87424947+shakirri@users.noreply.github.com>
2021-09-23 00:56:01 +0600

    Updated to a custom CSS

--- ledger/ledger/urls.py
+++ ledger/ledger/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -22,4 +24,4 @@ urlpatterns = [
     path('login', include('login.urls')),
     path('', include('inventory.urls')),
     path('admin/', admin.site.urls)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
